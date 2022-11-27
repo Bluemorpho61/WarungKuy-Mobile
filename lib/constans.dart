326d1f4f0591d3b60cc0d6b45f14b1944ac6662b
Azmikun1 <94949385+Azmikun1@users.noreply.github.com>
2022-11-27 10:54:42 +0700

    Add files via upload

---
+++ lib/constans.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Palette {
+  static Color bg1 = Color.fromRGBO(12, 82, 127, 1);
+  static Color bg2 = Color.fromRGBO(33, 150, 243, 1);
+  static Color star1 = Color.fromRGBO(242, 216, 114, 1);
+
+}
+
+TextStyle poppinsTextStyle = GoogleFonts.poppins();
