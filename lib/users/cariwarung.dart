0a5007868d8117123031f3a9b693c9e24885bd26
11edomantolas <11edomantolas@gmail.com>
2022-12-21 09:23:56 +0700

    fitur search sudah dikerjakan tapi belum selesai

--- lib/users/cariwarung.dart
+++ lib/users/cariwarung.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:warungkuy_mobile/constans.dart';
 import 'package:warungkuy_mobile/constans.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class Cariwarung extends StatefulWidget {
   @override
@@ -8,6 +9,10 @@ class Cariwarung extends StatefulWidget {
 }
 
 class _CariwarungState extends State<Cariwarung> {
+  void updateList(String value) {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,21 +27,34 @@ class _CariwarungState extends State<Cariwarung> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: TextFormField(
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: Colors.white,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          hintText: 'Cari warung di sini',
-          contentPadding: EdgeInsets.all(0),
-          prefixIcon: Icon(Icons.search, color: Colors.black,),
-        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                onChanged: (value) => updateList(value),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Cari warung di sini',
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            ]),
       ),
-    ),  
-       
     );
   }
 }
-   
