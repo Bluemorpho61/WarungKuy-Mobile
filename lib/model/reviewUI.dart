3037d21302f9007e08a4b898d22a231806625589
Azmikun1 <94949385+Azmikun1@users.noreply.github.com>
2022-12-23 09:13:33 +0700

    Add files via upload
    Penambahan Baru

---
+++ lib/model/reviewUI.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_star_rating_null_safety/smooth_star_rating_null_safety.dart';
+import 'package:warungkuy/constans.dart';
+
+class ReviewUI extends StatelessWidget {
+  final String image, name, comment;
+  final double rating;
+  final void Function() onTap;
+  final bool isLess;
+  const ReviewUI({
+    Key? key,
+    required this.image,
+    required this.name,
+    required this.comment,
+    required this.rating,
+    required this.onTap,
+    required this.isLess,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 2.0, bottom: 2.0, left: 16.0, right: 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 40.0,
+                width: 40.0,
+                margin: EdgeInsets.only(right: 16.0),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(image),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(44.0),
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  name,
+                  style: poppinsTextStyle.copyWith(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8.0),
+          Row(
+            children: [
+              SmoothStarRating(
+                starCount: 5,
+                rating: rating,
+                size: 28.0,
+                color: Colors.orange,
+                borderColor: Colors.orange,
+              ),
+            ],
+          ),
+          SizedBox(height: 8.0),
+          GestureDetector(
+            onTap: onTap,
+            child: isLess
+                ? Text(
+                    comment,
+                    style: poppinsTextStyle.copyWith(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  )
+                : Text(
+                    comment,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    style: poppinsTextStyle.copyWith(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+          )
+        ],
+      ),
+    );
+  }
+}
